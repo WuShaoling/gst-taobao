d8af91604ce1809f928a0591e260fc0bc25060ac
wsl <wu12490@gmail.com>
2018-04-16 12:19:53 +0800

    experiment hive is ok

---
+++ hadoop2-hadoop/spark-config/spark-env.sh
@@ -0,0 +1,11 @@
+export SPARK_HOME=/usr/spark/spark
+export SCALA_HOME=/esr/spark/scala
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export HADOOP_HOME=/usr/spark/hadoop
+export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SCALA_HOME/bin
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export YARN_CONF_DIR=$YARN_HOME/etc/hadoop
+export SPARK_MASTER_IP=localhost
+SPARK_LOCAL_DIRS=/usr/spark/spark
+SPARK_DRIVER_MEMORY=1G
+export SPARK_LIBARY_PATH=.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$HADOOP_HOME/lib/native
